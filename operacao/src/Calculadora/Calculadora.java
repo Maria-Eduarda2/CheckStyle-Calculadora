4c0903773780df5520a6c18a50e088f1e85de59c
Maria-Eduarda2 <104529452+Maria-Eduarda2@users.noreply.github.com>
2025-03-23 22:47:04 -0300

    Add files via upload

---
+++ operacao/src/Calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+public final class Calculadora {
+  /**
+   * Somar dois valores e retornar o resultado.
+   * Este método recebe dois números, soma e retorna o valor
+   * @param valor1 o primeiro número
+   *
+   * @param valor2 o segundo número
+   * @return a soma de valor1 + valor2 */
+  public double somar(final double valor1, final double valor2) {
+    return valor1 + valor2;
+  }
+  /**
+   * Subtrair o segundo valor do primeiro e retorna o resultado.
+   * Este método recebe dois números, subtrai o segundo pelo primeiro
+   * retorna o valor
+   * @param valor1 o número de onde será subtraído
+   * @param valor2 o número a ser subtraído
+   * @return a subtração do valor1 - valor2 */
+  public double subtrair(final double valor1, final double valor2) {
+    return valor1 - valor2;
+  }
+  /**
+   * Multiplica o segundo número do primeiro e retorna o resultado.
+   * Este método recebe dois números, multiplica o segundo pelo primeiro
+   * retorna o valor
+   * @param valor1 o primeiro número
+   * @param valor2 o segundo número
+   * @return a multiplicação de valor1 * valor2
+   */
+  public double multiplicar(final double valor1, final double valor2) {
+    return valor1 * valor2;
+  }
+  /**
+   * Divide o segundo número do primeiro e retorna o resultado.
+   * Este método recebe dois números, divide o segundo pelo primeiro
+   * retorna o valor
+   * @param valor1 o primeiro número
+   * @param valor2 o segundo número
+   * @return a divisão do valor1 / valor2 */
+  public double dividir(final double valor1, final double valor2) {
+    return valor1 / valor2;
+  }
+}
